43c1d20105cd26c3e778e18e8c4988f1764b19c9
Arham khalid <arhamkhalid79@gmail.com>
2026-07-30 10:50:09 +0500

    changes

--- Components/Reviews.tsx
+++ Components/Reviews.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Reviews = () => {
   return (
-    <section className='w-full h-screen relative'>
-        <img src="/img/bg-avh-standard.png" className='absolute top-0 left-0 h-240 bg-cover w-full ' alt="" />
+    <section className='w-full h-screen relative bg-white z-10'>
+        <img src="/img/bg-avh-standard.png" className='absolute top-0 left-0 h-260 bg-cover w-full ' alt="" />
 
         <div className='w-full h-full flex justify-center items-center text-8xl text-white'>
             <div className='flex z-0'>
@@ -11,9 +11,9 @@ const Reviews = () => {
                 <h2>The AVH <span>Standard</span></h2>
             </div>
         </div>
-        <div className='flex gap-3 px-24 relative -top-100'>
+        <div className='flex gap-3 px-24 bg-white  '>
             <div className='w-full h-full  flex justify-center z-0'>
-                <div className='w-136 h-full flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[45%] rounded-2xl'>
+                <div className='w-136 h-100 flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[73%] rounded-2xl'>
                     <div className='w-110 h-90 flex flex-col-reverse  relative'>
                         <div className='absolute w-full h-120 z-0 flex '>
                             <img src="/img/avh-standard-tile-img.png" className='absolute -top-8 left-0' alt="" />
@@ -28,8 +28,8 @@ const Reviews = () => {
                     </div>
                 </div>
             </div>
-            <div className='w-full h-100  flex justify-center z-0'>
-                <div className='w-136 h-full flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[55%] rounded-2xl'>
+            <div className='w-full h-full  flex justify-center z-0'>
+                <div className='w-136 h-100 flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[80%] rounded-2xl'>
                     <div className='w-110 h-90 flex flex-col-reverse  mt-18 relative'>
                         <div className='absolute w-full h-120 z-0 flex '>
                             <img src="/img/avh-standard-tile-img.png" className='absolute -top-8 left-0' alt="" />
@@ -45,7 +45,7 @@ const Reviews = () => {
                 </div>
             </div>
             <div className='w-full h-full flex justify-center z-0'>
-                <div className='w-136 h-full flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[45%] rounded-2xl'>
+                <div className='w-136 h-100 flex flex-col justify-center items-center px-10 bg-linear-to-b from-[#fbfaf8] to-[#f0eee6] absolute top-[73%] rounded-2xl'>
                     <div className='w-110 h-90 flex flex-col-reverse relative'>
                         <div className='absolute w-full h-120 z-0 flex '>
                             <img src="/img/avh-standard-tile-img.png" className='absolute -top-8 left-0' alt="" />
@@ -63,28 +63,31 @@ const Reviews = () => {
 
         </div>
         
-        <div className='w-full h-200 bg-white flex flex-col items-center justify-center gap-y-15 -mt-12'>
-            <div className='w-0.5 h-50 bg-[#4a4a4a]'></div>
-            <div className='w-full flex flex-col justify-center items-center gap-y-8 '>
-                <div className='flex items-center text-[#4a4a4a] gap-5'>
-                    <p className='text-8xl  text-[40px] md:text-[60px] lg:text-[80px] font-thin italic leading-none capitalize'>most hair looks good</p>
-                    <div className='relative w-[140px] md:w-[206px] h-[60px] md:h-[86px] rounded-[43px] overflow-hidden shrink-0'>
-                        <img src="/img/quote-img-1.png" width="100%" height="100%" className='bg-cover bg-[#EF4848]' alt="" />
+        <div className='w-full h-260 bg-white  '>
+            <div className='h-full flex flex-col items-center justify-center pt-60 pb-16 gap-y-15'>
+                    
+                <div className='w-0.5 h-50 bg-[#4a4a4a]'></div>
+                <div className='w-full flex flex-col justify-center items-center gap-y-8 '>
+                    <div className='flex items-center text-[#4a4a4a] gap-5'>
+                        <p className='text-8xl  text-[40px] md:text-[60px] lg:text-[80px] font-thin italic leading-none capitalize'>most hair looks good</p>
+                        <div className='relative w-[140px] md:w-[206px] h-[60px] md:h-[86px] rounded-[43px] overflow-hidden shrink-0'>
+                            <img src="/img/quote-img-1.png" width="100%" height="100%" className='bg-cover bg-[#EF4848]' alt="" />
+                        </div>
+                    </div>
+                    <div className='flex items-center text-[#4a4a4a] gap-5'>
+                        <div className='relative w-[140px] md:w-[206px] h-[60px] md:h-[86px] rounded-[43px] overflow-hidden shrink-0'>
+                            <img src="/img/quote-img-2.png" width="100%" height="100%" className='bg-cover bg-[#E3CC98]' alt="" />
+                        </div>
+                        <p className='text-8xl  text-[40px] md:text-[60px] lg:text-[80px] font-thin italic leading-none capitalize'>most hair looks good</p>
                     </div>
                 </div>
-                <div className='flex items-center text-[#4a4a4a] gap-5'>
-                    <div className='relative w-[140px] md:w-[206px] h-[60px] md:h-[86px] rounded-[43px] overflow-hidden shrink-0'>
-                        <img src="/img/quote-img-2.png" width="100%" height="100%" className='bg-cover bg-[#E3CC98]' alt="" />
-                    </div>
-                    <p className='text-8xl  text-[40px] md:text-[60px] lg:text-[80px] font-thin italic leading-none capitalize'>most hair looks good</p>
+                <div className='text-5xl mt-15'>
+                    <span>Perform Over Time</span>
                 </div>
-            </div>
-            <div className='text-5xl mt-15'>
-                <span>Perform Over Time</span>
-            </div>
-            <div>
-                <button className="btn-all h-14 w-60 text-m font-medium rounded-3xl capitalize text-white">begin your concultation</button>
+                <div>
+                    <button className="btn-all h-14 w-60 text-m font-medium rounded-3xl capitalize text-white">begin your concultation</button>
 
+                </div>
             </div>
         </div>
 
